cf0d77d95400adc44dc8f3708d2c2b1218302c03
Wassim Khemici <wkhemici@berkeley.edu>
2019-08-28 14:26:31 -0700

    background color

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,6 +31,8 @@ function main() {
     }
   }
 
+
+
   function updateDisplay() {
     document.getElementById("user-text").innerHTML = wins;
     document.getElementById("user-text2").innerHTML = losses;
@@ -38,23 +40,36 @@ function main() {
     document.getElementById("user-text4").innerHTML = guessesSoFar;
     document.getElementById("user-text5").innerHTML = computerGuess;
     document.getElementById("user-text6").innerHTML = userGuess;
+
+    if (guessesSoFar >= 10) {
+      console.log("Exceeded turn limit.");
+      console.log(wins);
+      // originalValues();
+      // updateDisplay();
+      // alert("End of game! Keep pressing keys for more rounds. ");
+
+    }
+
+
   }
+
+
+
 
   function originalValues() {
     wins = 0;
     losses = 0;
     guessesSoFar = 0;
     guessesLeft = 10;
-    computerGuess = ("Press a Key to see my guess");
-    userGuess = ("Press a key!");
-
+    computerGuess = ("Press a key to see my guess!");
+    userGuess = ("Press a key to guess a letter!");
 
   }
   //letterGuesser();
   //  $( function() {
   //   $( "#dialog" ).dialog();
   // } );
- 
+
 
   $(document).on('keypress', function () { //handler
 
@@ -65,18 +80,30 @@ function main() {
     compareGuess();
     updateDisplay();
 
- 
 
-    if (guessesSoFar > 10) {
+
+    if (guessesSoFar >= 10) {
       console.log("Exceeded turn limit.");
       console.log(wins);
       originalValues();
       updateDisplay();
-      alert("End of game! Keep pressing keys for more rounds. ");
-   
-     
-      
+      //we are going to try a timeout to slow down the alert
+      setTimeout(myFunction, 500); 
+      function myFunction() {
+      alert("End of game! Keep pressing keys for more rounds. ")};
 
+
+{/* <p>Click "Try it". Wait 3 seconds, and the page will alert "Hello".</p>
+
+<button onclick="setTimeout(myFunction, 3000);">Try it</button>
+
+<script>
+function myFunction() {
+  alert('Hello');
+}
+</script> */}
+
+      //try modal then timeOut 
     }
 
 
